fc358270e6cdc27d1420f080991d101e219930c3
Biff <saah1798t3l@gmail.com>
2025-12-28 15:52:59 -0500

    Initital Commit

---
+++ src/net/biff/Main.java
@@ -0,0 +1,10 @@
+package net.biff;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main{
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Solver solver = new Solver();
+    }
+}
---
+++ src/net/biff/Solver.java
@@ -0,0 +1,77 @@
+package net.biff;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solver {
+    public List<String> words = new ArrayList<>();
+    private List<String> alphabet = new ArrayList<>();
+    private short[] scores;
+    public Solver() throws IOException, URISyntaxException {
+        var url = new URI("https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/c46f451920d5cf6326d550fb2d6abb1642717852/wordle-answers-alphabetical.txt").toURL();
+        var http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("GET");
+        http.setConnectTimeout(5000);
+        http.setReadTimeout(5000);
+        if (http.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("Failed to access the internet (HTTP error code: " + http.getResponseCode() + ").");
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line);
+            }
+        }
+        for (char c = 'a'; c <= 'z'; c++){
+            alphabet.add(String.valueOf(c));
+        }
+        scores = new short[26];
+        for (int i = 0; i < 26; i++){
+            scores[i] = 0;
+        }
+        for (String word : words){
+            getWordCount(word);
+        }
+        for (int i = 0; i <26; i++){
+            System.out.println(alphabet.get(i) +" "+scores[i]);
+        }
+        duplicates().forEach(System.out::println);
+    }
+    private void getWordCount(String word){
+        for (char letter : word.toCharArray()){
+            scores[alphabet.indexOf(String.valueOf(letter))]++;
+        }
+    }
+    private List<Character> duplicates(){
+        List<Character> repeated = new ArrayList<>();
+        for (String word: words){
+            List<Character> through = new ArrayList<>();
+            for (char c : word.toCharArray()){
+                if (!through.contains(c)){
+                    through.add(c);
+                } else if (!repeated.contains(c)){
+                    repeated.add(c);
+                }
+            }
+        }
+        return repeated;
+    }
+    private void triplicates(){
+        List<Character> repeated = new ArrayList<>();
+        for (String word: words){
+            for (char c : word.toCharArray()){
+                Pattern letter = Pattern.compile(String.valueOf(c));
+                Matcher matcher = letter.matcher(word);
+                int reps = matcher.groupCount();
+
+            }
+        }
+    }
+}
